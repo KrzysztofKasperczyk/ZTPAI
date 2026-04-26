e402ffcdfdada6b9a9adda54ea2434328ab034e7
Krzysztof Kasperczyk <krzysztofkasperczyk505@gmail.com>
2026-04-26 12:18:46 +0200

    Book entity

---
+++ src/main/java/com/example/demo/entity/Book.java
@@ -0,0 +1,32 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "books")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(unique = true, nullable = false)
+    private String isbn;
+
+    private Integer year;
+
+    private Double price;
+}
